06edacfee1cfc3f4490d7f041e788e657ec6f2f2
T. Lanette <TLPollard4516@gmail.com>
2020-04-12 19:56:55 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,9 @@ import './styles/Header.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
 export const Header = () => (
-  <Jumbotron fluid className="jumbotron">
-    <h1 className="header-title text-center">
-        <img src='../../public/tribalrebelcowlogo.png' alt="Rebel-Cow Logo" />
-        Markdown Previewer</h1>
-    <hr />
-  </Jumbotron>
-);
+    <Jumbotron fluid className="jumbotron">
+        <h1 className="header-title text-center">Markdown Previewer</h1>
+        <hr />
+     </Jumbotron>
+    );
 
