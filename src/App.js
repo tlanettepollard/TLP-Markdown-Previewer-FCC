54e60433535aaa86fcb2974330669c7972fef5f3
T. Lanette <TLPollard4516@gmail.com>
2020-04-12 18:18:13 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import './index.css';
 
-import { Header } from './components/Header';
+import  {Header}  from '../src/components/Header';
 
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import './styles/Header.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
-const Header = () => (
-    <Jumbotron className='jumbotron'>
-        <h1 className='header-title text-center'>
-            Markdown Previewer
-        </h1>
-        <hr/>
-    </Jumbotron>
+export const Header = () => (
+  <Jumbotron fluid className="jumbotron">
+    <h1 className="header-title text-center">Markdown Previewer</h1>
+    <hr />
+  </Jumbotron>
 );
 
-export default Header;
