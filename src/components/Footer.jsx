b0e80b654b8b83cee07989d8da3a53124789c6dd
T. Lanette <TLPollard4516@gmail.com>
2020-04-13 15:25:46 -0400

    Updated Editor and Preview
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './styles/Footer.css';
 
+
+
 export const Footer = () => (
-         <footer className="col-xs-8 col-xs-offset-2 footer">
-           <hr />
-           <p className="text-center">
-             Markdown Previewer created by{" "}
-             <a href="https://github.com/TLanetteRose">
-               {" "}
-               <span>T.Lanette Pollard</span>
-             </a>
-           </p>
-           <p>FreeCodeCamp Front End Libraries Project </p>
+         <footer className="footer">
+             <p className="text-center">
+               Markdown Previewer created by{" "}
+               <a href="https://github.com/TLanetteRose">
+                 {" "}
+                 <span>T.Lanette Pollard</span>
+               </a>
+             </p>
+             <p>FreeCodeCamp Front End Libraries Project </p>
          </footer>
        );
