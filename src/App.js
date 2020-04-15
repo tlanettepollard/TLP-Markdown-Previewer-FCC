2bd741fa98854d725ec7160f6eec1421f1810477
T. Lanette <TLPollard4516@gmail.com>
2020-04-15 16:29:26 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import marked from 'marked';
 import './App.css';
 import './index.css';
@@ -6,85 +7,73 @@ import './index.css';
 import {Header} from '../src/components/Header';
 import {Footer} from '../src/components/Footer';
 
+//Allows line breaks with the return button
+marked.setOptions({
+  breaks: true
+});
 
 const renderer = new marked.Renderer();
 renderer.link = function (href, title, text) {
-  // eslint-disable-next-line 
-  return `a target="_blank" href="${href}">${text}` + '</a>';
+  // eslint-disable-next-line
+  return `<a target="_blank" href="${href}">${text}` + '</a>';
 }
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      markdown: placeholder,
-      editorMaximized: false,
-      previewMaximized: false
-    }
+      markdown: placeholder
+    };
     this.handleChange = this.handleChange.bind(this);
-    this.handleEditorMaximize = this.handleEditorMaximize.bind(this);
-    this.handlePreviewMaximize = this.handlePreviewMaximize.bind(this);
   }
-  handleChange(e) {
+  handleChange(e){
     this.setState({
       markdown: e.target.value
     });
   }
-  handleEditorMaximize() {
-    this.setState({
-      editorMaximized: !this.state.editorMaximized
-    });
-  }
-  handlePreviewMaximize() {
-    this.setState({
-      previewMaximized: !this.state.previewMaximized
-    });
-  }
   render() {
-    const classes = this.state.editorMaximized ? ['editorWrap maximized', 'previewWrap hide', 'fa fa-compress'] : ['editorWrap', 'previewWrap', 'fa fa-arrows-alt'];
-    return ( 
-    <div className = "App" >
-        <div>
-          < Header />
+    const classes = this.state.editorMaximized ? ['editorWrap maximized', 'previewWrap hide', 'fa fa-compress'] : this.state.previewMaximized ? ['editorWrap hide', 'previewWrap maximized','fa fa-compress'] : ['editorWrap', 'previewWrap', 'fa fa-arrows-alt'];
+    return (
+      <div>
+        <Header />
+        <div className="appWrap">
           <div className={classes[0]}>
-            <Toolbar icon={classes[2]} onClick={this.handleEditorMaximize} text="Editor" />
+            <Toolbar icon={classes[2]}onClick={this.handleEditorMaximize}text="Editor" />
             <Editor markdown={this.state.markdown} onChange={this.handleChange} />
           </div>
           <div className="converter"></div>
           <div className={classes[1]}>
             <Toolbar icon={classes[2]} onClick={this.handlePreviewMaximize} text="Previewer" />
-            <Preview markdown={this.state.markdown} />
+            <Previewer markdown={this.state.markdown} />
           </div>
-          < Footer />
         </div>
-      
-
-    </div>
+        <Footer />
+      </div>
     )
   }
-}; 
+};
 
 const Toolbar = (props) => {
   return (
-    <div title="rebel-cow" className="toolbar">
-      <i className="fa fa-free-code-camp"/>
+    <div className="toolbar">
+      <i title="rebel-cow" className="fa fa-free-code-camp"/>
       {props.text}
       <i onClick={props.onClick} className={props.icon}></i>
     </div>
-  )
-}
+  );
+};
 
 const Editor = (props) => {
   return (
     <textarea id="editor" value={props.markdown} onChange={props.onChange} type="text" />
-  )
-}
+  );
+};
 
-const Preview = (props) => {
+const Previewer = (props) => {
   return (
-    <div id="preview" dangerouslySetInnerHTML={{__html: marked(props.markdown, {renderer: renderer})}} />
-  )
-}
+    <div id="preview" dangerouslySetInnerHTML={{ __html: marked(props.markdown, {renderer: renderer })}} />
+  );
+};
+
 
 const placeholder = `# Welcome to my React Markdown Previewer!
 
@@ -130,7 +119,10 @@ And here. | Okay. | I think we get it.
 - Even if you use dashes or asterisks.
 * And last but not least, let's not forget embedded images:
 
-![React Logo w/ Text](https://goo.gl/Umyytc)`
+![React Logo w/ Text](https://goo.gl/Umyytc)`;
+
 
 
 export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
