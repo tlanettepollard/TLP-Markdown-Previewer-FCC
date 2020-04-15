947caa96bc9c6ad6c50e65d7eed2529fe8768d07
T. Lanette <TLPollard4516@gmail.com>
2020-04-14 20:44:36 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,22 +2,63 @@ import React from 'react';
 import marked from 'marked';
 import './App.css';
 import './index.css';
-import Container from 'react-bootstrap/Container';
 
 import {Header} from '../src/components/Header';
 import {Footer} from '../src/components/Footer';
+import { findAllByPlaceholderText } from '@testing-library/react';
 
 
 
-const App = () => {
-  return (
-    <div className="App">
-      <Container fluid>
-        <Header />
-        <Footer />
-      </Container>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: placeholder,
+      editorMaximized: false,
+      previewMaximized: false
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleEditorMaximize = this.handleEditorMaximize.bind(this);
+    this.handlePreviewMaximize = this.handlePreviewMaximize.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      markdown: e.target.value
+    });
+  }
+  handleEditorMaximize() {
+    this.setState({
+      editorMaximized: !this.state.editorMaximized
+    });
+  }
+  handlePreviewMaximize() {
+    this.setState({
+      previewMaximized: !this.state.previewMaximized
+    });
+  }
+  render() {
+    const classes = this.state.editorMaximized ? ['editorWrap maximized', 'previewWrap hide', 'fa fa-compress'] : ['editorWrap', 'previewWrap', 'fa fa-arrows-alt'];
+    return ( 
+    <div className = "App" >
+      <Header />
+        <div>
+          <div className={classes[0]}>
+            <Toolbar icon={classes[2]} onClick={this.handleEditorMaximize} text="Editor" />
+            <Editor markdown={this.state.markdown} onChange={this.handleChange} />
+          </div>
+          <div className="converter"></div>
+          <div className={classes[1]}>
+            <Toolbar icon={classes[2]} onClick={this.handlePreviewMaximize} text="Previewer" />
+          </div>
+        </div>
+      <Footer />
+
     </div>
-  )
-}
+    )
+  }
+}; 
+
+
+
 
 export default App;
